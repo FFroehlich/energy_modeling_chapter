8fa794697e9f14bc43219a9e71326071e737bb50
FFroehlich <fabian@schaluck.com>
2022-07-20 16:45:38 +0200

    update versions

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 from setuptools import setup
 
 setup(name='energy_modeling',
-      version='0.0.1',
+      version='1.0.0',
       description='book chapter about energy modeling in pysb',
       author='',
       packages=[],
       install_requires=[
             'numpy',
-            'petab',
-            'amici>=0.11.7',
-            'pypesto>=0.0.11',
+            'petab==0.1.27',
+            'amici==0.11.32',
+            'pypesto==0.2.13',
+            'fides==0.7.5',
             'pandas',
             'matplotlib',
             'pysb@https://github.com/FFroehlich/pysb@energy_modeling',
       ],
-      python_requires='>=3.6',
+      python_requires='>=3.8',
       zip_safe=False)
